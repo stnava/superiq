8618d1fd1c72cdebedcd8a83d0e5c196248d83ef
stnava <stnava@gmail.com>
2021-01-25 07:30:08 -0500

    WIP: superiq package basics

---
+++ superiq/__init__.py
@@ -0,0 +1,8 @@
+
+try:
+    from .version import __version__
+except:
+    pass
+
+from .super_resolution_segmentation_per_label import super_resolution_segmentation_per_label
+from .super_resolution_segmentation_per_label import ljlf_parcellation
