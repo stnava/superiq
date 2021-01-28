69e3667db5074b064659cab9090b641ab547ad60
stnava <stnava@gmail.com>
2021-01-28 15:56:44 -0500

    ENH: eval DTK with SR option

--- applications/evaluate_DKT_BF.py
+++ applications/evaluate_DKT_BF.py
@@ -41,15 +41,29 @@ overlaps = []
 seg_params={'submask_dilation': 20, 'reg_iterations': [100, 50, 0],
 'searcher': 0, 'radder': 2, 'syn_sampling': 16, 'syn_metric': 'mattes',
 'max_lab_plus_one': False, 'verbose': True}
-
+sr_params={"upFactor": [2,2,2], "dilation_amount": 12, "verbose":True}
+doSR = False
 for k in range( len( brains ) ):
      brainsLocal=brains.copy()
      brainsSegLocal=brainsSeg.copy()
      del brainsLocal[k:(k+1)]
      del brainsSegLocal[k:(k+1)]
      wlab = [75,76]
+     if doSR:
+        srseg = super_resolution_segmentation_per_label(
+            imgIn = imgIn,
+            segmentation = initlab0,
+            upFactor = sr_params['upFactor'],
+            sr_model = mdl,
+            segmentation_numbers = wlab,
+            dilation_amount = sr_params['dilation_amount'],
+            verbose = sr_params['verbose']
+        )
+        use_image = srseg['super_resolution']
+     else:
+         useg_image=ants.image_read(brains[k])
      localbf = basalforebrain_segmentation(
-       target_image=ants.image_read(brains[k]),
+       target_image=use_image,
        segmentation_numbers = wlab,
        template = ants.image_read(templatefilename),
        template_segmentation = ants.image_read(templatesegfilename),
@@ -64,4 +78,4 @@ for k in range( len( brains ) ):
      ants.image_write( ants.image_read(brains[k]), '/tmp/temp3.nii.gz' )
      overlaps.append( myol )
 
-# FIXME - organize the overlap output and write to csv     
+# FIXME - organize the overlap output and write to csv
