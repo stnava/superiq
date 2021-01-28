e8082948a92b647d81be3f46af5dcd6cd5f4ad6a
EC2 Default User <ec2-user@ip-172-31-78-104.base.invicro-aws.com>
2021-01-28 16:09:06 +0000

    Cleaned up dir

--- tests/test_basalforebrain.py
+++ tests/test_basalforebrain.py
@@ -11,7 +11,7 @@ import numpy.testing as nptest
 import unittest
 import ants
 from superiq import basalforebrain
-from superiq.pipeline utils import *
+from superiq.pipeline_utils import *
 
 
 def run_tests():
@@ -85,23 +85,6 @@ class TestModule_basalforebrain(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree("data")
         shutil.rmtree("outputs")
-
-
-class TestModule_sort_library_by_similarity(unittest.TestCase):
-    def test_sort_library_by_similarity_order(self):
-        targetimage = ants.image_read( ants.get_data("r16") )
-        img0 = ants.add_noise_to_image( targetimage, "additivegaussian", ( 0, 2 ) )
-        img1 = ants.image_read( ants.get_data("r27") )
-        img2 = ants.image_read( ants.get_data("r16") ).add_noise_to_image( "additivegaussian", (0, 6) )
-        tseg = ants.threshold_image( targetimage, "Otsu" , 3 )
-        img0s = ants.threshold_image( img0, "Otsu" , 3 )
-        img1s = ants.threshold_image( img1, "Otsu" , 3 )
-        img2s = ants.threshold_image( img2, "Otsu" , 3 )
-        ilist=[img2,img1,img0]
-        slist=[img2s,img1s,img0s]
-        ss = superiq.sort_library_by_similarity( targetimage, tseg, [3], ilist, slist )
-        testit = ss['ordering'][1] == 0
-        self.assertTrue( testit )
 
 if __name__ == "__main__":
     run_tests()
