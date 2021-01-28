81411c83ed8761e07add08e60ee91618e7e614a9
stnava <stnava@gmail.com>
2021-01-28 12:47:42 -0500

    BUG: threads stuff should not be set in function

--- superiq/basalforebrain.py
+++ superiq/basalforebrain.py
@@ -1,8 +1,4 @@
 import os
-threads = "8"
-os.environ["TF_NUM_INTEROP_THREADS"] = threads
-os.environ["TF_NUM_INTRAOP_THREADS"] = threads
-os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = threads
 import os.path
 from os import path
 import glob as glob
@@ -19,7 +15,7 @@ import pandas as pd
 from superiq import super_resolution_segmentation_per_label
 from superiq import ljlf_parcellation
 from superiq import check_for_labels_in_image
-from superiq.pipeline_utils import * 
+from superiq.pipeline_utils import *
 from superiq import list_to_string
 
 def basalforebrain(
@@ -35,22 +31,22 @@ def basalforebrain(
             "wlab":[75,76], "submask_dilation":20, "reg_iteration": [100,50,10],
             "searcher": 2, "radder": 3, "syn_sampling": 2, "syn_metric": "CC",
             "max_lab_plus_one": False, "verbose": True}
-        ): 
+        ):
     """
     Complete script for running super resolution on specific labels and ljlf
     parcellation on the ouputs. A config is the prefered input with the
     necessary parameter values. If a config is not provided, the other
-    arguments are used to specify the inputs. 
+    arguments are used to specify the inputs.
 
     Arguments
     ---------
     config : string or dict
-        a string containing the json config contents, a string of the 
+        a string containing the json config contents, a string of the
         relative path to the config file, or a python dict of the json data
 
     templatefilename : string
         path to the template image
-    
+
     templatesegfilename : string
         path to the template labels image
 
@@ -69,14 +65,14 @@ def basalforebrain(
     sr_params : dict
         dict containing the variable parameters for the super resolution call.
         The parameters are: { "upFactor" : list, "dilation_amount": int, "verbose" : bool}
-    
+
     seg_params : dict
         dict containing the variable parameters for the ljlf parcellation call.
         The parameters are:
             {"wlab":list, "submask_dilation":int, "reg_iteration": list,
             "searcher": int, "radder": int, "syn_sampling": int, "syn_metric": string,
             "max_lab_plus_one": bool, "verbose": bool}
-   
+
     Example
     -------
     <With Config>
@@ -95,16 +91,16 @@ def basalforebrain(
                 "wlab":[75,76], "submask_dilation":20, "reg_iteration": [100,50,10],
                 "searcher": 2, "radder": 3, "syn_sampling": 2, "syn_metric": "CC",
                 "max_lab_plus_one": False, "verbose": True
-            }) 
+            })
     """
-    if config: 
+    if config:
         config = LoadConfig(config)
         output_filename = "outputs/" + config.output_name
-        # TODO  
+        # TODO
         templatefilename = get_s3_object(config.template_bucket, config.template_key, "data")
-        # TODO 
+        # TODO
         templatesegfilename = get_s3_object(config.template_bucket, config.template_label_key, "data")
-        # TODO 
+        # TODO
         infn = get_pipeline_data(
                 "brain_ext-bxtreg_n3.nii.gz",
                 config.input_value,
@@ -112,33 +108,33 @@ def basalforebrain(
                 config.pipeline_prefix,
         )
 
-        # TODO 
+        # TODO
         model_file_name = get_s3_object(config.model_bucket, config.model_key, "models")
         # TODO
         sr_params = config.sr_params
         seg_params = config.seg_params
-   
+
         # TODO
         if seg_params['wlab']['range']:
             wlab = range(seg_params['wlab']['values'][0],seg_params['wlab']['values'][1])
         else:
             wlab = seg_params['wlab']['values']
-        
-        # TODO 
+
+        # TODO
         atlas_image_keys = list_images(config.atlas_bucket, config.atlas_image_prefix)
         brains = [get_s3_object(config.atlas_bucket, k, "atlas") for k in atlas_image_keys]
         brains.sort()
-        # TODO 
+        # TODO
         atlas_label_keys = list_images(config.atlas_bucket, config.atlas_label_prefix)
         brainsSeg = [get_s3_object(config.atlas_bucket, k, "atlas") for k in atlas_label_keys]
         brainsSeg.sort()
-   
+
     else:
         brains = glob.glob(atlas_images_dir+"/*")
         brains.sort()
         brainsSeg = glob.glob(atlas_labels_dir+"/*")
         brainsSeg.sort()
-        wlab = seg_params['wlab'] 
+        wlab = seg_params['wlab']
         output_filename = "outputs/basalforebrain"
 
     # input data
@@ -146,19 +142,19 @@ def basalforebrain(
     template = ants.image_read( templatefilename )
     templateL = ants.image_read( templatesegfilename )
     mdl = tf.keras.models.load_model( model_file_name ) # FIXME - parameterize this
-    
+
     havelabels = check_for_labels_in_image( wlab, templateL )
-    
+
     if not havelabels:
         raise Exception("Label missing from the template")
-    
+
     # expected output data
     output_filename_sr = output_filename + "_SR.nii.gz"
     output_filename_sr_seg_init = output_filename  +  "_SR_seginit.nii.gz"
     output_filename_sr_seg = output_filename  +  "_SR_seg.nii.gz"
     output_filename_sr_seg_csv = output_filename  + "_SR_seg.csv"
-    
-    
+
+
     # first, run registration - then do SR in the local region
     if not 'reg' in locals():
         print("Registration")
@@ -166,11 +162,11 @@ def basalforebrain(
         forward_transforms = reg['fwdtransforms']
         initlab0 = ants.apply_transforms( imgIn, templateL,
               forward_transforms, interpolator="genericLabel" )
-    else: 
+    else:
         forward_transforms = reg
         initlab0 = ants.apply_transforms( imgIn, templateL,
               forward_transforms, interpolator="genericLabel" )
-    
+
     srseg = super_resolution_segmentation_per_label(
         imgIn = imgIn,
         segmentation = initlab0,
@@ -180,13 +176,13 @@ def basalforebrain(
         dilation_amount = sr_params['dilation_amount'],
         verbose = sr_params['verbose']
     )
-    
+
     # write
     initlab0 = ants.apply_transforms( srseg['super_resolution'], templateL,
         forward_transforms, interpolator="genericLabel" )
     ants.image_write( srseg['super_resolution'] , output_filename_sr )
     ants.image_write( initlab0 , output_filename_sr_seg_init )
-    
+
     locseg = ljlf_parcellation(
             srseg['super_resolution'],
             segmentation_numbers=wlab,
@@ -215,7 +211,7 @@ def basalforebrain(
       ants.resample_image_to_target(probs[whichprob76], srseg['super_resolution'] ),
       0.3, 1.0 )
     ants.image_write( probseg,  output_filename_sr_seg )
-   
+
     if config:
         get_label_geo(
                 probseg,
@@ -236,7 +232,7 @@ def basalforebrain(
             config.process_name,
             env=config.environment,
         )
-        
+
 
 if __name__ == "__main__":
     print('Starting basalforebrain')
