a63153350e9539a0e70b8651c01f143766dfb3cb
stnava <stnava@gmail.com>
2021-01-28 13:32:23 -0500

    BUG: wrong import file

--- superiq/__init__.py
+++ superiq/__init__.py
@@ -11,5 +11,5 @@ from .super_resolution_segmentation_per_label import ljlf_parcellation_one_templ
 from .super_resolution_segmentation_per_label import sort_library_by_similarity
 from .super_resolution_segmentation_per_label import list_to_string
 from .basalforebrain import basalforebrainSR
-from .basalforebrain import basalforebrainOR
+from .basalforebrainOR import basalforebrainOR
 from .pipeline_utils import *
