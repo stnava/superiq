2e0860421f58b6237186261f97d9f92808068126
EC2 Default User <ec2-user@ip-172-31-79-186.base.invicro-aws.com>
2021-01-29 19:12:35 +0000

    Added or script

--- superiq/basalforebrain.py
+++ superiq/basalforebrain.py
@@ -186,3 +186,148 @@ def basalforebrainSR(
     #    config.process_name,
     #    env=config.environment,
     #)
+
+def basalforebrainOR(
+        input_image,
+        template,
+        templateL,
+        atlas_images,
+        atlas_labels,
+        forward_transforms=None,
+        wlab=[75,76],
+        seg_params={
+            "submask_dilation":20, "reg_iteration": [100,50,10],
+            "searcher": 2, "radder": 3, "syn_sampling": 2, "syn_metric": "CC",
+            "max_lab_plus_one": False, "verbose": True}
+        ):
+    """
+    # TODO
+
+    Arguments
+    ---------
+    input_image : ANTsImage
+        the input n3 image
+
+    template : ANTsImage
+        the template image
+
+    templateL : ANTsImage
+        the template labels image
+
+    atlas_images : list (ANTsImages)
+        the sorted list of atlas images
+
+    atlas_labels : list (ANTsImages)
+        the sorted list of atlas label images
+
+    forward_transforms : list (str)
+        a list with the paths to the warp image and transform matrix if using 
+        preregistered input image
+
+    wlab : list (int)
+        list of labels of interest in the target image
+
+    seg_params : dict
+        dict containing the variable parameters for the ljlf parcellation call.
+        The parameters are:
+            {"submask_dilation":int, "reg_iteration": list,
+            "searcher": int, "radder": int, "syn_sampling": int, "syn_metric": string,
+            "max_lab_plus_one": bool, "verbose": bool}
+
+    Example
+    -------
+    # TODO 
+    """
+    brains = atlas_images
+    brainsSeg = atlas_labels
+    output_filename = "outputs/basalforebrainOR"
+
+    imgIn = input_image
+
+    havelabels = check_for_labels_in_image( wlab, templateL )
+
+    if not havelabels:
+        raise Exception("Label missing from the template")
+    
+    # Dont need this
+    output_filename_or = output_filename + "_OR.nii.gz"
+    output_filename_or_seg_init = output_filename  +  "_OR_seginit.nii.gz"
+    output_filename_or_seg = output_filename  +  "_OR_seg.nii.gz"
+    output_filename_or_seg_csv = output_filename  + "_OR_seg.csv"
+
+    if forward_transforms is None:
+        print("Registration")
+        reg = ants.registration( imgIn, template, 'SyN' )
+        forward_transforms = reg['fwdtransforms']
+        initlab0 = ants.apply_transforms( imgIn, templateL,
+              forward_transforms, interpolator="genericLabel" )
+    else:
+        initlab0 = ants.apply_transforms( imgIn, templateL,
+              forward_transforms, interpolator="genericLabel" )
+
+    ants.image_write( init_tx , output_filename_or_seg_init )
+    imgOR = ants.iMath(imgIn, "Normalize")
+
+    localbf = basalforebrain_segmentation( 
+            target_image=imgOR,
+            segmentation_numbers=wlab,
+            template=template,
+            template_segmentation=templateL, 
+            library_intensity=atlas_images,
+            library_segmentation=atlas_labels,
+            seg_params=seg_params,
+    )
+    
+    # Don't think we need this anymore
+    #locseg = ljlf_parcellation(
+    #        srseg['super_resolution'],
+    #        segmentation_numbers=wlab,
+    #        forward_transforms=forward_transforms,
+    #        template=template,
+    #        templateLabels=templateL,
+    #        library_intensity = brains,
+    #        library_segmentation = brainsSeg,
+    #        submask_dilation=seg_params['submask_dilation'],  # a parameter that should be explored
+    #        searcher=seg_params['searcher'],  # double this for SR
+    #        radder=seg_params['radder'],  # double this for SR
+    #        reg_iterations=seg_params['reg_iterations'], # fast test
+    #        syn_sampling=seg_params['syn_sampling'],
+    #        syn_metric=seg_params['syn_metric'],
+    #        max_lab_plus_one=seg_params['max_lab_plus_one'],
+    #        output_prefix=output_filename,
+    #        verbose=seg_params['verbose'],
+    #    )
+    #probs = locseg['ljlf']['ljlf']['probabilityimages']
+    #probability_labels = locseg['ljlf']['ljlf']['segmentation_numbers']
+    # find proper labels
+    #whichprob75 = probability_labels.index(wlab[0])
+    #whichprob76 = probability_labels.index(wlab[1])
+    #probseg = ants.threshold_image(
+    #  ants.resample_image_to_target(probs[whichprob75], srseg['super_resolution'] ) +
+    #  ants.resample_image_to_target(probs[whichprob76], srseg['super_resolution'] ),
+    #  0.3, 1.0 )
+   
+    probseg = ants.threshold_image(localbf['probsum'], 0.5, 2)
+    ants.image_write( probseg,  output_filename_or_seg )
+
+     
+    # Keep for reference but do not belong here
+    #get_label_geo(
+    #        probseg,
+    #        srseg['super_resolution'],
+    #        config.process_name,
+    #        config.input_value,
+    #        resolution="SR",
+    #)
+    #plot_output(
+    #    srseg['super_resolution'],
+    #    "outputs/basalforebrain-SR_ortho_plot.png",
+    #    probseg,
+    #)
+    #handle_outputs(
+    #    config.input_value,
+    #    config.output_bucket,
+    #    config.output_prefix,
+    #    config.process_name,
+    #    env=config.environment,
+    #)
