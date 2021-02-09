68d87aa168fbc34c528b369803846aac8a675a46
EC2 Default User <ec2-user@ip-172-31-65-33.base.invicro-aws.com>
2021-02-09 19:35:26 +0000

    Updated prepare volumes

--- applications/prepare_volumes.py
+++ applications/prepare_volumes.py
@@ -3,10 +3,12 @@ from superiq.pipeline_utils import *
 import pandas
 import os
 import boto3
+import multiprocessing as mp
+
 
 bucket = "eisai-basalforebrainsuperres2"
-metadata = "metadata/full_metadata.csv"
-version = "v03"
+metadata = "metadata/full_metadata_20210208.csv"
+version = "v01"
 prefix = f"superres-pipeline-{version}/ADNI/"
 stack_filename = f'stacked_bf_volumes_{version}.csv'
 expanded_filename = f'expanded_bf_volumes_{version}.csv'
@@ -20,69 +22,78 @@ expand = False
 pivot =  True
 merge = True
 
+def get_files(k):
+    bucket = "eisai-basalforebrainsuperres2"
+    path = get_s3_object(bucket, k, "tmp") 
+    df = pd.read_csv(path)
+    fields = ["Label", 'VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
+    df = df[fields]      
+    new_rows = []
+    for i,r in df.iterrows():
+        label = int(r['Label'])
+        fields = ['VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
+        select_data = r[fields] 
+        values = select_data.values
+        field_values = zip(fields, values)
+        for f in field_values:
+            new_df = {}
+            new_df['Measure'] = f[0]
+            new_df['Value'] = f[1]
+            new_df['Label'] = label
+            new_df = pd.DataFrame(new_df, index=[0])
+            new_rows.append(new_df)
+
+    df = pd.concat(new_rows)
+    filename = path.split('/')[-1]
+    split = filename.split('-')
+    name_list = ["Project", "Subject", "Date", "Modality", "Repeat", "Process", "Name"]
+    zip_list = zip(name_list, split)
+    for i in zip_list:
+        df[i[0]] = i[1]
+    df['OriginalOutput'] = "-".join(split[:5]) + ".nii.gz"
+    os.remove(path) 
+    return df 
 
 if stack:
     keys = list_images(bucket, prefix)
     
     keys = [i for i in keys if ".csv" in i]
-    
-    dfs = []
-    for k in keys:
-        print(k)
-        path = get_s3_object(bucket, k, "tmp") 
-        df = pd.read_csv(path)
-        fields = ["Label", 'VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
-        df = df[fields]      
-        new_rows = []
-        for i,r in df.iterrows():
-            label = int(r['Label'])
-            fields = ['VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
-            select_data = r[fields] 
-            values = select_data.values
-            field_values = zip(fields, values)
-            for f in field_values:
-                new_df = {}
-                new_df['Measure'] = f[0]
-                new_df['Value'] = f[1]
-                new_df['Label'] = label
-                new_df = pd.DataFrame(new_df, index=[0])
-                new_rows.append(new_df)
+    with mp.Pool() as p:
+        dfs = p.map(get_files, keys)
+    #for k in keys:
+    #    path = get_s3_object(bucket, k, "tmp") 
+    #    df = pd.read_csv(path)
+    #    fields = ["Label", 'VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
+    #    df = df[fields]      
+    #    new_rows = []
+    #    for i,r in df.iterrows():
+    #        label = int(r['Label'])
+    #        fields = ['VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
+    #        select_data = r[fields] 
+    #        values = select_data.values
+    #        field_values = zip(fields, values)
+    #        for f in field_values:
+    #            new_df = {}
+    #            new_df['Measure'] = f[0]
+    #            new_df['Value'] = f[1]
+    #            new_df['Label'] = label
+    #            new_df = pd.DataFrame(new_df, index=[0])
+    #            new_rows.append(new_df)
 
-        df = pd.concat(new_rows)
-        filename = path.split('/')[-1]
-        split = filename.split('-')
-        name_list = ["Project", "Subject", "Date", "Modality", "Repeat", "Process", "Name"]
-        zip_list = zip(name_list, split)
-        for i in zip_list:
-            df[i[0]] = i[1]
-        df['OriginalOutput'] = "-".join(split[:5]) + ".nii.gz"
-        dfs.append(df)
-        os.remove(path) 
+    #    df = pd.concat(new_rows)
+    #    filename = path.split('/')[-1]
+    #    split = filename.split('-')
+    #    name_list = ["Project", "Subject", "Date", "Modality", "Repeat", "Process", "Name"]
+    #    zip_list = zip(name_list, split)
+    #    for i in zip_list:
+    #        df[i[0]] = i[1]
+    #    df['OriginalOutput'] = "-".join(split[:5]) + ".nii.gz"
+    #    dfs.append(df)
+    #    os.remove(path) 
     
     stacked = pd.concat(dfs)
     stacked.to_csv(stack_filename, index=False)
-    #s3.upload_file(stack_filename, bucket, stack_filename)
 
-#if expand:
-#    lgms = pd.read_csv(stack_filename)
-#    lgms['Name'] = [i.split('.')[0] for i in lgms['Name']]
-#    new_rows = []
-#    for i,r in lgms.iterrows():
-#        label = int(r['Label'])
-#        fields = ['VolumeInMillimeters', 'SurfaceAreaInMillimetersSquared'] 
-#        select_data = r[fields] 
-#        values = select_data.values
-#        field_values = zip(fields, values)
-#        for f in field_values:
-#            new_df = {}
-#            new_df['Measure'] = f[0]
-#            new_df['Value'] = f[1]
-#            new_df['Label'] = label
-#            new_df = pd.DataFrame(new_df, index=[0])
-#            new_rows.append(new_df)
-#    new_df = pd.concat(new_rows)
-#    new_df.to_csv(expanded_filename, index=False)
-#    s3.upload_file(expanded_filename, bucket, expanded_filename)
 
 if pivot:
     df = pd.read_csv(stack_filename)
@@ -110,7 +121,8 @@ if merge:
     meta = get_s3_object(bucket, metadata, "tmp")
     metadf = pd.read_csv(meta)
     os.remove(meta) 
-    merge = pd.merge(metadf, data, how="right", left_on="new_filename", right_on="OriginalOutput")
+    merge = pd.merge(metadf, data, how="right", left_on="filename", right_on="OriginalOutput",suffixes=("","_x"))
+    merge.drop(['Repeat_x'], inplace=True, axis=1)
     merge.to_csv(merge_filename, index=False)
     s3.upload_file(merge_filename, bucket, s3_prefix + merge_filename)
 
