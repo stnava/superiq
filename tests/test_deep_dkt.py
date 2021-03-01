f3a8abe403face19d97221c125cbacf352a2f780
EC2 Default User <ec2-user@ip-172-31-78-95.base.invicro-aws.com>
2021-03-01 18:52:06 +0000

    Tests successful

--- tests/test_deep_dkt.py
+++ tests/test_deep_dkt.py
@@ -21,18 +21,31 @@ class TestDeepDKT(unittest.TestCase):
         )
         template = antspynet.get_antsxnet_data("biobank")
         self.template = ants.image_read(template)
+
+        self.sr_model = get_s3_object(
+            "invicro-pipeline-inputs",
+            "models/SEGSR_32_ANINN222_3.h5",
+            "/tmp"
+        )
+
+        self.seg_numbers = [1006, 1007, 1015, 1016,]
+        self.sr_params = {
+            "upFactor": [2,2,2,],
+            "dilation_amount": 6,
+            "verbose": True,
+        }
+
     def test_deep_dkt(self):
         input_params={
             "target_image": ants.image_read(self.input_image),
-            "segmentation_numbers": [],
+            "segmentation_numbers": self.seg_numbers,
             "template": self.template,
-            "library_intensity": "",
-            "library_segmentation": "",
-            "seg_params": {},
-            "forward_transforms": None,
-            "output_filename": None,
+            "sr_model": self.sr_model,
+            "sr_params": self.sr_params,
+            "output_path": "/tmp/deep_dkt/",
         }
         output = superiq.deep_dkt(**input_params)
+        print(output) 
         self.assertTrue(output)
 
 
