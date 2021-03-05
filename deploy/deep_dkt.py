b0b0a89582f46fc37e3c7c2eb2e3fd9987af4257
EC2 Default User <ec2-user@ip-172-31-3-124.base.invicro-aws.com>
2021-03-05 20:36:45 +0000

    new eval script

--- deploy/deep_dkt.py
+++ deploy/deep_dkt.py
@@ -15,17 +15,32 @@ from superiq import deep_dkt
 
 def main(input_config):
     config = LoadConfig(input_config)
-    original_image_path = config.input_value
+    if config.environment == 'prod':
+        original_image_path = config.input_value
 
-    input_image = get_pipeline_data(
-            "brain_ext-bxtreg_n3.nii.gz",
-            original_image_path,
-            config.pipeline_bucket,
-            config.pipeline_prefix,
-            "/data"
-    )
+        input_image = get_pipeline_data(
+                "brain_ext-bxtreg_n3.nii.gz",
+                original_image_path,
+                config.pipeline_bucket,
+                config.pipeline_prefix,
+                "/data"
+        )
 
-    input_image = ants.image_read(input_image)
+        input_image = ants.image_read(input_image)
+    elif config.environment == 'val':
+        input_path = get_s3_object(config.input_bucket, config.input_value, '/data')
+        input_image = ants.read_image(input_path)
+
+        image_base_name = input_image.split('/')[-1].split('.')[0]
+        image_label_name = \
+            config.atlas_label_prefix +  image_base_name + '_JLFSegOR.nii.gz'
+        image_labels_path = get_s3_object(
+            config.atlas_label_bucket,
+            image_label_name,
+            "/data",
+        )
+    else:
+        raise ValueError(f"The environemnt {config.environment} is not recognized")
 
     wlab = config.wlab
 
@@ -33,7 +48,7 @@ def main(input_config):
     template = ants.image_read(template)
 
     sr_model = get_s3_object(config.model_bucket, config.model_key, "/data")
-    #mdl = tf.keras.models.load_model(model_path)
+    mdl = tf.keras.models.load_model(model_path)
 
     output_path = "/outputs/"
     if not os.path.exists(output_path):
@@ -50,13 +65,88 @@ def main(input_config):
 
     output = deep_dkt(**input_params)
 
-    handle_outputs(
-        config.input_value,
-        config.output_bucket,
-        config.output_prefix,
-        config.process_name,
-        output_path,
-    )
+    if config.environment == 'prod':
+        handle_outputs(
+            config.input_value,
+            config.output_bucket,
+            config.output_prefix,
+            config.process_name,
+            output_path,
+        )
+
+    elif config.evironment == 'val':
+
+        nativeGroundTruth = ants.image_read(image_labels_path)
+        nativeGroundTruth = ants.mask_image(
+            nativeGroundTruth,
+            nativeGroundTruth,
+            level = wlab,
+            binarize=False
+        )
+        gtSR = super_resolution_segmentation_per_label(
+            imgIn = ants.iMath(input_image, "Normalize"),
+            segmentation = nativeGroundTruth, # usually, an estimate from a template, not GT
+            upFactor = config.sr_params['upFactor'],
+            sr_model = mdl,
+            segmentation_numbers = wlab,
+            dilation_amount = config.sr_params['dilation_amount'],
+            verbose = config.sr_params['verbose']
+        )
+        nativeGroundTruthProbSR = gtSR['probability_images'][0]
+        nativeGroundTruthSR = gtSR['super_resolution_segmentation']
+        nativeGroundTruthBinSR = ants.mask_image(
+            nativeGroundTruthSR,
+            nativeGroundTruthSR,
+            wlab,
+            binarize=True
+        )
+        ######
+
+        srsegLJLF = ants.threshold_image(output['srSeg']['probsum'], 0.5, math.inf )
+        nativeOverlapSloop = ants.label_overlap_measures(
+            nativeGroundTruth,
+            output['nativeSeg']['segmentation']
+        )
+        srOnNativeOverlapSloop = ants.label_overlap_measures(
+            nativeGroundTruthSR,
+            output['srOnNativeSeg']['super_resolution_segmentation']
+        )
+        srOverlapSloop = ants.label_overlap_measures(
+            nativeGroundTruthSR,
+            output['srSeg']['segmentation']
+        )
+        srOverlap2 = ants.label_overlap_measures( nativeGroundTruthBinSR, srsegLJLF )
+
+        brainName = []
+        dicevalNativeSeg = []
+        dicevalSRNativeSeg = []
+        dicevalSRSeg = []
+
+        brainName.append(target_image_base)
+        dicevalNativeSeg.append(nativeOverlapSloop["MeanOverlap"][0])
+        dicevalSRNativeSeg.append( srOnNativeOverlapSloop["MeanOverlap"][0])
+        dicevalSRSeg.append( srOverlapSloop["MeanOverlap"][0])
+
+        dict = {
+                'name': brainName,
+                'diceNativeSeg': dicevalNativeSeg,
+                'diceSRNativeSeg': dicevalSRNativeSeg,
+                'diceSRSeg': dicevalSRSeg
+        }
+        df = pd.DataFrame(dict)
+        path = f"{image_base_name}_dice_scores.csv"
+        df.to_csv("/tmp/" + path, index=False)
+        s3 = boto3.client('s3')
+        s3.upload_file(
+            "/tmp/" + path,
+            config.output_bucket,
+            config.output_prefix + path,
+        )
+
+
+
+else:
+        pass
 
 if __name__ == "__main__":
     config = sys.argv[1]
