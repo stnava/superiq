fd56e1d216ee9a79f86cebe5b1060ce48fb98330
EC2 Default User <ec2-user@ip-172-31-8-131.base.invicro-aws.com>
2021-03-10 15:54:29 +0000

    Collect volumes

---
+++ applications/collect_ppmi_volumes.py
@@ -0,0 +1,15 @@
+from superiq import VolumeData
+
+if __name__ == "__main__":
+      bucket = "mjff-ppmi"
+      #metadata_key = "volume_measures/data_w_metadata_v01.csv"
+      version = "ljlf-left"
+      prefix = f"superres-pipeline-{version}/"
+      stack_filename = f'stacked_volumes_{version}.csv'
+      pivoted_filename = f'pivoted_bf_volumes_{version}.csv'
+      #merge_filename = f"dkt_with_metdata_{version}.csv"
+      upload_prefix = "volume_measures/"
+      vd = VolumeData(bucket, prefix, upload_prefix, cache=False)
+      local_stack = vd.stack_volumes(stack_filename)
+      local_pivot = vd.pivot_data(local_stack, pivoted_filename)
+
--- superiq/__init__.py
+++ superiq/__init__.py
@@ -17,3 +17,4 @@ from .basalforebrain_seg import native_to_superres_ljlf_segmentation
 from .pipeline_utils import *
 from .deep_dkt import deep_dkt
 from .deep_hippo import deep_hippo
+from .collect_volume_files import VolumeData
