8bdd5f421778e0c588a918591265bb9c17ede047
EC2 Default User <ec2-user@ip-172-31-78-204.base.invicro-aws.com>
2021-03-16 15:28:56 +0000

    New volume collectoin script

--- applications/collect_ppmi_volumes_v2.py
+++ applications/collect_ppmi_volumes_v2.py
@@ -4,6 +4,22 @@ import boto3
 import pandas as pd
 from datetime import datetime
 
+def collect_brain_age():
+	bucket = "mjff-ppmi"
+	version = "simple"
+	prefix = f"superres-pipeline-{version}/"
+	objects = list_images(bucket, prefix)
+	brain_age = [i for i in objects if i.endswith('brain_age.csv')]
+	dfs = []
+	for i in brain_age:
+		ba = get_s3_object(bucket, i, '/tmp')
+		filename = ba.split('/')[-1]
+		splits = filename.split('-')
+		ba_df = pd.read_csv(ba)
+		ba_df['Repeat'] = splits[4]
+		dfs.append(ba_df)
+	dfs = pd.concat(dfs)
+	return dfs
 
 if __name__ == "__main__":
 	bucket = "mjff-ppmi"
@@ -19,7 +35,11 @@ if __name__ == "__main__":
 	local_pivot = vd.pivot_data(local_stack, pivoted_filename)
 	local_pivot_df = pd.read_csv(local_pivot)
 	local_pivot_df = local_pivot_df
+	ba = collect_brain_age()
 	local_pivot_df['join_date'] = [str(i)[:6] for i in local_pivot_df['Date']]
+	print(local_pivot_df.shape)
+	local_pivot_df = pd.merge(local_pivot_df, ba, on='Repeat')
+	print(local_pivot_df.shape)
 	s3 = boto3.client('s3')
 	local_pivot_df.to_csv('local_pivot.csv')
 	s3.upload_file('local_pivot.csv', bucket, "simple_reg_sr_ppmi_volumes.csv")
