e5fcb7d46da58c4307fe8fab038a92f081f2ecd6
EC2 Default User <ec2-user@ip-172-31-74-13.base.invicro-aws.com>
2021-03-24 18:31:43 +0000

    Threads fix

--- deploy/brain_ext.py
+++ deploy/brain_ext.py
@@ -2,7 +2,7 @@
 import os.path
 from os import path
 
-threads = "8"
+threads = os.environ['cpu_threads']
 # set number of threads - this should be optimized per compute instance
 os.environ["TF_NUM_INTEROP_THREADS"] = threads
 os.environ["TF_NUM_INTRAOP_THREADS"] = threads
