3136f9688d6963b926058cb515a9803318e98a44
EC2 Default User <ec2-user@ip-172-31-70-72.base.invicro-aws.com>
2021-04-23 20:17:03 +0000

    Rand basis is working

--- deploy/randombasis_projection.py
+++ deploy/randombasis_projection.py
@@ -9,7 +9,7 @@ import functools
 from operator import mul
 from sklearn.utils.extmath import randomized_svd
 import ia_batch_utils as batch
-
+import pandas as pd
 from multiprocessing import Pool
 
 # for repeatability
@@ -22,55 +22,40 @@ def myproduct(lst):
 def main(input_config):
     random.seed(0)
     c = input_config
-    print(c.input_value)
-    randbasis = batch.get_s3_object(c.rha_bucket, c.rha_key, 'data')
-    randbasis = ants.image_read(randbasis).numpy()
-    nvox = [c.nvox]*c.dimensionality
-    print(nvox)
-    print(randbasis.shape)
-    print(f"Random basis dimensions: {randbasis.ndim}")
-    print(f"Basis prod(nvox): {myproduct(nvox)}")
-    #if randbasis.ndim != myproduct(nvox):
-    #    raise ValueError("Rand basis dimensions to match the voxel product")
+    nvox = c.nvox
     nBasis = c.nbasis
     X = np.random.rand( nBasis*2, myproduct( nvox ) )
 
-    U, Sigma, randbasis = randomized_svd(X,
-                                  n_components=nBasis,
-                                  random_state=None)
+    U, Sigma, randbasis = randomized_svd(
+        X,
+        n_components=nBasis,
+        random_state=None
+    )
+    if randbasis.shape[1] != myproduct(nvox):
+        raise ValueError("columns in rand basis do not match the nvox product")
 
-    accepted_transforms = ["Rigid", "Affine", "Similarity", "SyN"]
-    if c.registration_transform in accepted_transforms:
-        registration_transform = c.registration_transform
-    else:
-        raise ValueError(f"Expected registration_transform values [{*accepted_transforms,}], not {c.registration_transform}")
     randbasis = np.transpose( randbasis )
     rbpos = randbasis.copy()
     rbpos[rbpos<0] = 0
-    templatefn = batch.get_s3_object(c.template_bucket, c.template_key, 'data')
+    if hasattr(c, 'template_bucket'):
+        templatefn = batch.get_s3_object(c.template_bucket, c.template_key, 'data')
     imgfn = batch.get_s3_object(c.input_bucket, c.input_value, 'data')
     img = ants.image_read(imgfn)
     norm = ants.iMath(img, 'Normalize')
     resamp = ants.resample_image(norm, nvox, use_voxels=True)
-    #if registration_transform is not None and templatefn is not None: # FIXME - check this
-    template = ants.image_read( templatefn ).resample_image(nvox, use_voxels=True)
-    resamp = ants.registration( template, resamp, registration_transform )['warpedmovout']
+    if hasattr(c, 'registration_transform') and hasattr(c, "template_bucket"):
+        accepted_transforms = ["Rigid", "Affine", "Similarity", "SyN"]
+        if c.registration_transform in accepted_transforms:
+            registration_transform = c.registration_transform
+        else:
+            raise ValueError(f"Expected registration_transform values [{*accepted_transforms,}], not {c.registration_transform}")
+        template = ants.image_read( templatefn ).resample_image(nvox, use_voxels=True)
+        resamp = ants.registration( template, resamp, registration_transform )['warpedmovout']
     imat = ants.image_list_to_matrix([resamp], resamp*0+1)
     uproj = np.matmul(imat, randbasis)
     uprojpos = np.matmul(imat, rbpos)
     imgsum = resamp.sum()
 
-    #outputs
-    #split = c.input_value.split('/')
-    #subject = split[2]
-    #date = split[3]
-    #image_id = split[5]
-    #record = {
-    #    'Subject.ID':subject,
-    #    "Date":date,
-    #    'Image.ID':image_id,
-    #    "RandBasisImgSum": imgsum,
-    #}
     record = {}
     uproj_counter = 0
     for i in uproj[0]:
@@ -84,20 +69,13 @@ def main(input_config):
         record[name] = i
     df = pd.DataFrame(record, index=[0])
     fields = [i for i in df.columns if i.startswith('RandBasis')]
-    batch.write_outputs(
-        c.input_value,
-        c.process_name,
-        imgfn,
-        df,
+    batch.record_random_basis_projections(
         c.input_bucket,
         c.input_value,
+        df,
         c.resolution,
         c.batch_id,
-        fields=fields
     )
-
-# FIXME - this process should be generalized for 2D, 3D (maybe) 4D images
-# and with whatever project / modality we have on hand.
 
 if __name__ == "__main__":
     config = sys.argv[1]
